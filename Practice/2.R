14c899284de6fd6408c0da17a62d68487ef41338
Shyam Kumar Sodankoor <47209581+shyamks111@users.noreply.github.com>
2020-01-09 17:22:35 +0000

    Update and rename Assignments/Assignment2.R to Practice/2.R

--- Practice/2.R
+++ Practice/2.R
@@ -1,19 +1,27 @@
+# Linear Model as a list
 mod<-lm(eruptions ~ waiting, data=faithful)
+
+# Function to get the model for the given variable
 get_data<-function(list_mod,var){
   list_mod$model[[var]]
 }
+
 d<-get_data(mod,"waiting")
 print(d[1:20])
+
+# Function to get the model for the given variable
 get_coefficient<-function(list_mod,coeff){
   list_mod$coefficients[coeff]
 }
+
 c<-get_coefficient(mod,"waiting")
 print(c)
+
+# Creating a new summary function for the given list with the below mentioned variables
 mod_summary<-function(list_mod){
-  ##b<-c("Rows" = nrow(list_mod$model),"Cols" = ncol(list_mod$model))
-  ##names(b) <- c("Rows","Cols")
   a<-list(call=list_mod$call,coeff=list_mod$coefficients,DataSize=
             c("Rows" = nrow(list_mod$model),"Cols" = ncol(list_mod$model)))
 }
+
 s<-mod_summary(mod)
 print(s)
